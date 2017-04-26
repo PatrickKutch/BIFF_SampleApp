15640b2514c3addfe3bd306f40e244796fef3518
Patrick <Patrick.Kutch@gmail.com>
2017-04-26 07:01:30 -0700

    Added welcome screen and web widget demo

--- Marvin.bat
+++ Marvin.bat
@@ -1,1 +1,3 @@
 java -jar BIFF.Marvin.jar -i demo\demoapp.xml -v -log %computername%.html
+@REM With proxy
+#java -Dhttp.proxyHost=proxy.myproxy.company.com -Dhttp.proxyPort=911 -jar BIFF.Marvin.jar -i demo\demoapp.xml -v -log %computername%.html
