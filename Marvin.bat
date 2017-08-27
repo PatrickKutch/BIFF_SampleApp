ed798209e71c6c5e25417feeeefbe17b14064e3b
Patrick <Patrick.Kutch@gmail.com>
2017-08-27 12:13:55 -0700

    Structure update

--- Marvin.bat
+++ Marvin.bat
@@ -1,3 +1,3 @@
-java -jar BIFF.Marvin.jar -i demo\demoapp.xml -v -log %computername%.html
+java -jar BIFF.Marvin.jar -i SampleApplication\demoapp.xml -v -log %computername%.html
 @REM With proxy
-@rem -Dhttp.proxyHost=proxy.myproxy.company.com -Dhttp.proxyPort=911 -jar BIFF.Marvin.jar -i demo\demoapp.xml -v -log %computername%.html
+@rem -Dhttp.proxyHost=proxy.myproxy.company.com -Dhttp.proxyPort=911 -jar BIFF.Marvin.jar -i $(AppDir)\demoapp.xml -v -log %computername%.html
